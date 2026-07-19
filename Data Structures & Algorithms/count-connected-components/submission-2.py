1f8bf963e70e0aca721a65b9e62dc1f9a731b02b
Karan Sharma <64159670+KaranSharma18@users.noreply.github.com>
2026-07-19 15:03:28 +0530

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def build_undirected(self, n: int, edges: List[List[int]]) -> dict:
+        graph = defaultdict(list)
+        n_componenets = 0
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        return graph
+
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = self.build_undirected(n, edges)
+        visited = set()
+        n_componenets = 0
+        
+        def bfs(start):
+            stack = [start]
+            visited.add(start)
+
+            while stack:
+                node = stack.pop()
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+                        visited.add(neighbor)
+                    
+        for node in range(n):
+            if node not in visited:
+                n_componenets += 1
+                bfs(node)
+        return n_componenets
+        
